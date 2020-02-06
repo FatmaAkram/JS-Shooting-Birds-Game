b4297f1d1e7a0d7f5f3130460041293e4638c3c4
Fatma Akram <fatma.khater96@gmail.com>
2020-02-06 10:54:18 +0200

    gif added

--- js/app.js
+++ js/app.js
@@ -1,58 +1,51 @@
+$(function () {
+    class Duck {
+        game = document.getElementById("game");
 
-$(function(){
-    class Duck
-    {
-        game=document.getElementById("game");
-
-        constructor(left,top,speed,img)
-        {
-            this.bird=document.createElement("img");
-            this.bird.src=img;
-            this.bird.style.left=left+"px";
-            this.bird.style.top=top+"px";
+        constructor(left, top, speed, img) {
+            this.bird = document.createElement("img");
+            this.bird.src = img;
+            this.bird.style.left = left + "px";
+            this.bird.style.top = top + "px";
             game.appendChild(this.bird);
-            this.speed=speed;
-            
+            this.speed = speed;
+
         }
-         move()
-        {
-            var Bird=this.bird;
-            var left=parseInt(Bird.style.left);
-            var id= setInterval(function(){
+        move() {
+            var Bird = this.bird;
+            var left = parseInt(Bird.style.left);
+            var id = setInterval(function () {
                 console.log(left);
-                if(left>=781)
-                {
+                if (left >= 781) {
                     clearInterval(id);
-                    game.removeChild(Bird);
+//                    game.removeChild(Bird);
+                } else {
+                    left += 20;
+                    Bird.style.left = left + "px";
                 }
-                else{
-                    left+=20;
-                    Bird.style.left=left+"px";
-                }
-                
-            
-            },100);
-            
+
+
+            }, 100);
+
         }
-         
+
     }
 
     function getRndNumber(min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
-      }
-      
-      images=["Images/1.png","Images/2.png","Images/3.png"];     
-        var ducks=[];
-      for(let i=0;i<3;i++)
-      {
-        randNum=getRndNumber(1,800);
-        randbird=getRndNumber(1,3);
-        ducks[i]=new Duck(1,randNum,100,images[randbird]);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    images = ["images/normalBird.gif", "images/bonusBird.gif", "images/blackBird.gif"];
+    var ducks = [];
+    for (let i = 0; i < 3; i++) {
+        randNum = getRndNumber(1, 800);
+        randbird = getRndNumber(1, 3);
+        ducks[i] = new Duck(1, randNum, 100, images[randbird]);
         ducks[i].move();
-      }
+    }
     // setInterval(function(){
     //     $("#bird").animate({left:"+=200"},8000)
     //     $("#bird").animate({top:"+=200"},2000)
     // },7000);
-    
+
 });
